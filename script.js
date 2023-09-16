2330ea0b56fb93838cea6989d4f9a1928c5c7e1e
EvanioTech <112532056+EvanioTech@users.noreply.github.com>
2023-09-16 15:23:11 -0300

    finalizando a aula

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-var a;
-a = "cell";
-console.log (a)
+/*var a;
+a = 3 + 3;
+alunos
+console.log (a)*/
+
+
+var derickidade = 3;
+var idade = derickidade + 25 ;
+b = idade
+c = idade - 2
+d = c * 2 + 23
+e = d / 25
+console.log( "qual a idade de evanio?",idade)
+console.log("qual a idade de Talita?", c)
+console.log( "qual a idade da biza?",d)
+console.log("qual a idade de derick?",e)
+
